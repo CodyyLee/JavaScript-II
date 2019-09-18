bae312922244bc940bf09c94a242e4e72282f83f
Cody Garrett <cody.l.garrett22@gmail.com>
2019-09-18 07:54:32 -0600

    project complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,17 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+function parentFunction(){
+  let name = 'Cody';
 
+  function nameFunction()  {
+    console.log("Hello, my name is " + name);
+  }
+
+  nameFunction();
+}
+
+parentFunction();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
